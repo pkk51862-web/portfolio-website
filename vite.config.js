8cf6161597e6b3c25d934a7fb51c9446786f10c7
Pawares <pawarest.putkung@gmail.com>
2026-09-07 00:03:57 +0700

    update 4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/', // Replace 'portfolio-website' with your exact GitHub repo name
+  base: '/portfolio-website/',
 })
